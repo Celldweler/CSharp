c1eef835a636fbb85c4898cb116e2e58bbf784a4
Raime <0961120962fb@gmail.com>
2020-03-09 22:49:48 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+
+
+namespace CSharp
+{
+    class Program
+    {
+        IFormatProvider formatter = new NumberFormatInfo { NumberDecimalSeparator = "." };
+
+        static void SumDecimalDigit(double f_d, double s_d)
+        {
+            Console.WriteLine($"Sum digits {f_d}, {s_d} -> " + (double)((double)f_d + (double)s_d));
+
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("First number");
+            double f_d = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Second number");
+            double s_d = Convert.ToDouble(Console.ReadLine());
+            SumDecimalDigit(f_d, s_d);
+            Console.ReadKey();
+        }
+        
+    }
+}
+
