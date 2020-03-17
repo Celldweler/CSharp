de8d5ce05a0070d2e3b114de6a4ac3736b5d80e6
Raime <0961120962fb@gmail.com>
2020-03-17 16:13:42 +0200

    fix

--- Program.cs
+++ Program.cs
@@ -1,30 +1,72 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 using System.Globalization;
-
-
+//
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace CSharp
 {
     class Program
     {
-        IFormatProvider formatter = new NumberFormatInfo { NumberDecimalSeparator = "." };
 
-        static void SumDecimalDigit(double f_d, double s_d)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine($"Sum digits {f_d}, {s_d} -> " + (double)((double)f_d + (double)s_d));
 
+            
+            string path = @"C:\Users\King of maining\source\repos\CSharp\УсловиеЗадания.txt";
+
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            // асинхронное чтение
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                string line;
+                while ((line = await sr.ReadLineAsync()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            /*
+           Triangle t1 = new Triangle();
+
+           t1.SideA = 3;
+           t1.SideB = 4;
+           t1.SideC = 5;
+           Console.WriteLine(t1.ToString());
+           t1.PrintInfo();
+
+           Triangle t2 = new Triangle(2, 6, 5, "T1");
+           t2.PrintInfo();
+           */
+
+            int k = 3;
+            Triangle[] triangles = new Triangle[k];
+            Random r = new Random();
+            double square = 0;
+            int realTriangles = 0;
+
+            for (int i = 0; i < k; i++)
+            {
+                triangles[i] = new Triangle(r.NextDouble() * 10, r.NextDouble() * 10, r.NextDouble() * 10, "T" + (i + 1).ToString());
+                Console.WriteLine(triangles[i].ToString());
+                if (triangles[i].IsTriangle())
+                {
+                    square += triangles[i].GetSquare();
+                    realTriangles++;
+                }
+            }
+
+            Console.WriteLine("Average square of " + realTriangles + " triangles is: " + square / realTriangles);
         }
-        static void Main(string[] args)
-        {
-            Console.Write("First number");
-            double f_d = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine();
-            Console.Write("Second number");
-            double s_d = Convert.ToDouble(Console.ReadLine());
-            SumDecimalDigit(f_d, s_d);
-            Console.ReadKey();
-        }
-        
     }
 }
 
---
+++ Triangle.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class Triangle
+    {
+        double sideA, sideB, sideC;
+        String title;
+
+        public Triangle()
+        {
+        }
+
+        public Triangle(double sideA, double sideB, double sideC, string title = "Triangle")
+        {
+            this.sideA = sideA;
+            this.sideB = sideB;
+            this.sideC = sideC;
+            this.title = title;
+        }
+
+        public double SideA { get => sideA; set => sideA = value > 0 ? value : 0; }
+        public double SideB { get => sideB; set => sideB = value > 0 ? value : 0; }
+        public double SideC { get => sideC; set => sideC = value > 0 ? value : 0; }
+        public string Title { get => title; set => title = value; }
+
+        public bool IsTriangle()
+        {
+            bool result = false;
+            if (
+                (this.sideA + this.sideB > this.sideC) &&
+                (this.sideB + this.sideC > this.sideA) &&
+                (this.sideA + this.sideC > this.sideB)
+                )
+                result = true;
+
+            return result;
+        }
+
+        public double GetPerimeter()
+        {
+            return this.sideA + this.sideB + this.sideC;
+        }
+
+        public double GetSquare()
+        {
+            double p = this.GetPerimeter() / 2;
+            return Math.Sqrt(p * (p - this.sideA) * (p - this.sideB) * (p - this.sideC));
+        }
+
+        public double GetAngle(double sideA, double sideB, double sideC)
+        {
+            return (180 / Math.PI) * Math.Acos((sideA * sideA + sideB * sideB - sideC * sideC) / (2 * sideA * sideB));
+        }
+
+        public override String ToString()
+        {
+            return
+                this.IsTriangle()
+                ?
+                "Triangle: " + this.Title + " (" + this.SideA + ", " + this.SideB + ", " + this.SideC + ")" +
+                "\nPerimeter: " + this.GetPerimeter() +
+                "\nSquare: " + this.GetSquare() +
+                "\nAngles:\n" + this.GetAngle(this.SideA, this.SideB, this.SideC) +
+                "\n" + this.GetAngle(this.SideB, this.SideC, this.SideA) +
+                "\n" + this.GetAngle(this.SideA, this.SideC, this.SideB)
+                :
+                this.Title + " (" + this.SideA + ", " + this.SideB + ", " + this.SideC + ")" + " is NOT triangle"
+                ;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("Triangle: ({0}, {1}, {2})", this.SideA, this.SideB, this.SideC);
+            Console.WriteLine(this.IsTriangle() ? "Triangle" : "NOT triangle");
+            Console.WriteLine(this.GetPerimeter());
+            Console.WriteLine(this.GetSquare());
+            Console.WriteLine(this.GetAngle(this.SideA, this.SideB, this.SideC));
+            Console.WriteLine(this.GetAngle(this.SideB, this.SideC, this.SideA));
+            Console.WriteLine(this.GetAngle(this.SideA, this.SideC, this.SideB));
+
+            Console.WriteLine(this.ToString());
+        }
+    }
+}
